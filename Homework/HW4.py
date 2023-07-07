cce4fbd2d45d32a667586be468ab1971b5e74481
PYQA96 <astahov1996dpc@gmail.com>
2023-07-07 13:38:23 +0300

    changeHW4

--- Homework/HW4.py
+++ Homework/HW4.py
@@ -1,18 +1,16 @@
-stop = True
 Name = input("Введите имя ")
-while stop:
+while True:
     Age = input("Введите возраст ")
-    Age=int(Age)
-    if Age<0 or Age==0 or  not str(Age).isnumeric():
+    Age = int(Age)
+    if Age < 0 or Age == 0 or not str(Age).isnumeric():
         print("Ошибка, повторите ввод")
-    elif 0< Age < 10:
+    elif Age < 10:
         print(f"Привет, шкет {Name}")
-    elif 10<= Age <= 18:
+    elif Age <= 18:
         print(f"Как жизнь {Name}")
-    elif 18 < Age < 100 :
+    elif Age < 100:
         print(f"Что желаете {Name}")
     else:
         print(f"{Name} вы лжете - в наше время столько не живут...")
     Exit = input("Желаете выйти Д\Н: ")
-    stop = False if Exit.lower()=="д" else True
-
+    if Exit.lower() == "д": stop = False
