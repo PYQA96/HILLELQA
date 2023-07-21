72dbd7ce56bd09723976d799b784d6c1561e5537
PYQA96 <astahov1996dpc@gmail.com>
2023-07-21 12:33:40 +0300

    fixdz

--- Homework/HW15.py
+++ Homework/HW15.py
@@ -1,5 +1,5 @@
 def is_polindrom(value : list):
-   result_masiv=list(filter(lambda x: True if x[::-1]==x[::] and str(x).islower()==True else False,value))
+   result_masiv=list(filter(lambda x: True if str(x[::-1]).lower()==str(x[::]).lower()  else False,value))
    return result_masiv
 
 
