4257030d4591c784f3c639c41fb4ac4a902776a3
PYQA96 <astahov1996dpc@gmail.com>
2023-07-26 12:58:39 +0300

    Homeworks9-11 fix2

--- Homework/HW10.py
+++ Homework/HW10.py
@@ -1,10 +1,7 @@
 byte_string = b'r\xc3\xa9sum\xc3\xa9'
-#  UTF-8)
-decoded_string = byte_string.decode()
-print("Декодировано в строковый вид (кодировка по умолчанию):", decoded_string)
-#  'Latin1'
+decoded_string = byte_string.decode('latin1')
+print("(кодировка по умолчанию):", decoded_string)
 latin1_bytes = decoded_string.encode('latin1')
-print("Преобразовано обратно в байтовый вид (кодировка Latin1):", latin1_bytes)
-# (по умолчанию)
+print("(кодировка Latin1):", latin1_bytes)
 final_string = latin1_bytes.decode()
-print("Декодировано в строковый вид (кодировка по умолчанию):", final_string)
+print("(кодировка по умолчанию):", final_string)
--- Homework/HW9.py
+++ Homework/HW9.py
@@ -1,7 +1,11 @@
 def infinity_function():
     def descriptor(value):
         result = ""
-        if value.isdigit() and value not in [".", ","]:
+
+        value_for_check = value.replace(",", ".", 1)
+        if value_for_check[0] == "-": value_for_check = value_for_check.replace(value[0], "", 1)
+        print(value_for_check)
+        if value_for_check.isdigit():
             value = int(value)
 
             if value == 0:
@@ -10,9 +14,9 @@ def infinity_function():
                 result = f"Вы ввели положительное число {value}"
             else:
                 result = f"Вы ввели отрицательное  число {value}"
-        elif value.replace(".", "").replace(",", "").replace("-", "", 1).isdigit() and value.count(
-                ".") == 1 or value.count(",") == 1:
-            value = float(value)
+        elif value_for_check.count(".") == 1 and value_for_check.replace(".", "").isdigit() and value_for_check.count(
+                "-") == 0 and value_for_check[0].isdigit():
+            value = float(value.replace(",", "."))
             if value == 0:
                 result = f"Вы ввели  {0}"
             elif value > 0:
@@ -33,47 +37,54 @@ def infinity_function():
             continue
 
 
-# infinity_function()
+# 1) целые отрицательные числа, например -5
+#
+# 2) не верно определяются некоторые не корректные числа и часть из них
+# приводит к падению программы, что не желательно. Числа: "5-5", "--56", "3-3.43", "6..6", "..56"
+#
+# 3) у вас не верно определяются числа с запятой, а не точкой: "-6,7"
+#
+infinity_function()
+
 
 print("-" * 50)
 
-
 # так как я полюбтл рекурсию сделал и вторым вариантом
 
-def infinity_function1(var):
-    def descriptor(value):
-        result = ""
-        if value.isdigit() and value not in [".", ","]:
-            value = int(value)
-
-            if value == 0:
-                result = f"Вы ввели  {0}"
-            elif value > 0:
-                result = f"Вы ввели положительное число {value}"
-            else:
-                result = f"Вы ввели отрицательное  число {value}"
-        elif value.replace(".", "").replace(",", "").replace("-", "", 1).isdigit() and (
-                value.count(".") == 1 or value.count(",") == 1):
-            value = float(value)
-            if value == 0:
-                result = f"Вы ввели  {0}"
-            elif value > 0:
-                result = f"Вы ввели положительное дробное число {value}"
-            else:
-                result = f"Вы ввели отрицательное дробное число {value}"
-        else:
-            result = f"Вы shit {value}"
-        return result
-
-    if var.lower() in ["выход", "exit", "quit", "e", "q"]:
-        return print("Exit with function")
-    else:
-        print(descriptor(var))
-        print("Ввод новго числа ")
-        new_data = input()
-        return infinity_function1(new_data)
-
-
-print("Ввод числа")
-var = input()
-print(infinity_function1(var))
+# def infinity_function1(var):
+#     def descriptor(value):
+#         result = ""
+#         if value.isdigit() and value not in [".", ","]:
+#             value = int(value)
+#
+#             if value == 0:
+#                 result = f"Вы ввели  {0}"
+#             elif value > 0:
+#                 result = f"Вы ввели положительное число {value}"
+#             else:
+#                 result = f"Вы ввели отрицательное  число {value}"
+#         elif value.replace(".", "").replace(",", "").replace("-", "", 1).isdigit() and (
+#                 value.count(".") == 1 or value.count(",") == 1):
+#             value = float(value)
+#             if value == 0:
+#                 result = f"Вы ввели  {0}"
+#             elif value > 0:
+#                 result = f"Вы ввели положительное дробное число {value}"
+#             else:
+#                 result = f"Вы ввели отрицательное дробное число {value}"
+#         else:
+#             result = f"Вы shit {value}"
+#         return result
+#
+#     if var.lower() in ["выход", "exit", "quit", "e", "q"]:
+#         return print("Exit with function")
+#     else:
+#         print(descriptor(var))
+#         print("Ввод новго числа ")
+#         new_data = input()
+#         return infinity_function1(new_data)
+#
+#
+# print("Ввод числа")
+# var = input()
+# print(infinity_function1(var))
